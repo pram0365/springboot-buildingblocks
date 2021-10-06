bf0ae6e265ee2a0a1df651846d0eb45bb38bb95c
parshu <pram0365@gmail.com>
2021-10-06 23:25:00 +0530

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,7 +3,7 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Parshudff
+//Parshudffs
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
